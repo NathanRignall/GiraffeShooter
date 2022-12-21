b1979e382450107902002d056bd1534cb0f5f910
Nathan Rignall <rignallnathan@gmail.com>
2022-12-21 12:23:26 +0000

    Remove map context and ad demo collider

---
+++ GiraffeShooterClient/Container/Menu/MenuContext.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace GiraffeShooterClient.Container.Menu
+{
+    public class MenuContext
+    {
+        public MenuContext()
+        {
+        }
+
+
+    }
+}
--- GiraffeShooterClient/Entity/System/Collider.cs
+++ GiraffeShooterClient/Entity/System/Collider.cs
@@ -1,4 +1,4 @@
-
+using Microsoft.Xna.Framework;
 namespace GiraffeShooterClient.Entity
 {
     class Collider : Component
@@ -7,6 +7,44 @@ namespace GiraffeShooterClient.Entity
         public Collider()
         {
             ColliderSystem.Register(this);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+            // foreach (Collider collider in ColliderSystem.components)
+            // {
+            //     if (collider != this)
+            //     {   
+                    
+            //         // check if the two cubes are intersecting
+            //         if (collider.entity.GetComponent<Physics>().BoundingBox.Intersects(entity.GetComponent<Physics>().BoundingBox))
+            //         {
+            //             // write the collision to the console
+            //             System.Console.WriteLine("Collision detected between " + entity.name + " and " + collider.entity.name);
+
+            //             // write bounding box information to the console
+            //             System.Console.WriteLine("Entity 1: " + entity.GetComponent<Physics>().BoundingBox.ToString());
+            //             System.Console.WriteLine("Entity 2: " + collider.entity.GetComponent<Physics>().BoundingBox.ToString());
+
+            //             // calculate the objects momentum
+            //             Vector3 entityMomentum = entity.GetComponent<Physics>().velocity * entity.GetComponent<Physics>().mass;
+
+            //             // calculate the colliders momentum
+            //             Vector3 colliderMomentum = collider.entity.GetComponent<Physics>().velocity * collider.entity.GetComponent<Physics>().mass;
+
+            //             // calculate the total momentum
+            //             Vector3 totalMomentum = entityMomentum + colliderMomentum;
+
+            //             // calculate the total mass
+            //             float totalMass = entity.GetComponent<Physics>().mass + collider.entity.GetComponent<Physics>().mass;
+                        
+
+            //         }
+
+            //     }
+            // }
+
         }
 
         public override void Deregister()
